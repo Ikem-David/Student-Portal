8b0e3c144aafc82e4b0aec86f16ba0a2c34acf80
Ikem-David <ikemdavid8@icloud.com>
2025-06-13 11:04:19 +0100

    Change database connection text

--- Backend/db.py
+++ Backend/db.py
@@ -1,13 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
 
-# URL = "mysql+pymysql://fastapi_user:your_password@localhost/vehicle_tracking"
-
-# CREATE DATABASE (DB NAME);
-# CREATE USER 'USERNAME_user'@'localhost' IDENTIFIED BY 'your_password';
-# GRANT ALL PRIVILEGES ON DB NAME.* TO 'USERNAME_user'@'localhost';
-# FLUSH PRIVILEGES;
-
 db_url = "mysql+pymysql://fastapi_user:your_password@localhost/student"
 
 engine = create_engine(db_url)
